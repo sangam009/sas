e84ccba6857568c20271004309b7c38e96766cc9
rajgaurav.srivastava <rajgaurav.srivastava@servicenow.com>
2020-07-14 23:10:36 +0530

    Formatting and remove db insatnce

--- service/Users/users.js
+++ service/Users/users.js
@@ -1,33 +1,30 @@
-const mongo = require("../../utils/mongo");
-const user = require("../../model/user");
+const dbConnection = require('../../utils/dbConnection');
+const User = require('../../model/User');
 
 class Users {
-  async createUser(userObj) {
-    try {
-      let mongoClient = await mongo.getClient();
-      let modeltemp = new user(userObj);
-      let done = await modeltemp.save();
-      return done;
-    } catch (error) {
-      if (error.code == 11000) {
-        console.log("user already exists");
-        return userObj;
-      } else {
-        console.log(error.code);
-        throw new Error(error);
-      }
-    }
-  }
+	async createUser(userObj) {
+		try {
+			const modeltemp = new User(userObj);
+			const done = await modeltemp.save();
+			return done;
+		} catch (error) {
+			if (error.code == 11000) {
+				console.log('user already exists');
+				return userObj;
+			} 
+			console.log(error.code);
+			throw new Error(error);
+		}
+	}
 
-  async getUserBasedOnFilters(filter) {
-    try {
-      let mongoClient = await mongo.getClient();
-      let done = await user.find(filter);
-      return done;
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
+	async getUserBasedOnFilters(filter) {
+		try {
+			const done = await User.find(filter);
+			return done;
+		} catch (error) {
+			throw new Error(error);
+		}
+	}
 }
 
 module.exports = Users;
--- utils/callbacks.js
+++ utils/callbacks.js
@@ -1,26 +1,24 @@
-const FacebookStrategy = require("passport-facebook").Strategy;
-const User = require("../service/Users/users");
+const FacebookStrategy = require('passport-facebook').Strategy;
+const User = require('../service/Users/users');
 
 module.exports = {
-  facebook: (config) => {
-    return new FacebookStrategy(
-      config,
-      async (token, refreshToken, profile, done) => {
-        let user;
-        try {
-          let userObj = {
-            id: profile.id,
-            token,
-            email: (profile.emails[0].value || "").toLowerCase(),
-            name: `${profile.name.givenName} ${profile.name.familyName}`,
-            provider: "facebook",
-          };
-          user = await new User().createUser(userObj);
-          return done(null, user);
-        } catch (error) {
-          return done(error);
-        }
-      }
-    );
-  },
+	facebook: (config) => new FacebookStrategy(
+		config,
+		async (token, refreshToken, profile, done) => {
+			let user;
+			try {
+				const userObj = {
+					id: profile.id,
+					token,
+					email: (profile.emails[0].value || '').toLowerCase(),
+					name: `${profile.name.givenName} ${profile.name.familyName}`,
+					provider: 'facebook',
+				};
+				user = await new User().createUser(userObj);
+				return done(null, user);
+			} catch (error) {
+				return done(error);
+			}
+		},
+	),
 };
