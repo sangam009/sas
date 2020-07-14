6fddb86de582cf40831241f6324cbd2f4d13cb6d
rajgaurav.srivastava <rajgaurav.srivastava@servicenow.com>
2020-07-14 23:07:46 +0530

    establish db connection static including rajan PR

---
+++ utils/dbConnection.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const dbConnection = (function () {
+	const url = process.env.MONGO_DB_URL || 'mongodb+srv://admin:admin@cluster0.gt5ch.gcp.mongodb.net/sas'; // 'mongodb://localhost:27017';
+	let mongoClient; 
+
+	async function dbConnect() {
+		mongoClient = await mongoose.connect(url, {
+			useNewUrlParser: true,
+			poolSize: 5,
+		});
+		return mongoClient;
+	}
+
+	async function getdbClient() {
+		try {
+			if (mongoClient) {
+				console.log('db connection is already alive');
+				return mongoClient;
+			} 
+			console.log('Establishing new connection');
+			mongoClient = await dbConnect();
+			return mongoClient;
+		} catch (error) {
+			console.log(error);
+			throw new Error('unable to connect to the mongo db client');
+		}
+	}
+	return {
+		getdbClient,
+	};
+}());
+
+module.exports = dbConnection;
