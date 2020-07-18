9586f76eeaf76e3d97fdfc9d528cbd78e6d15d4d
sangam_5000979 <sangam.dubey@paytm.com>
2020-07-18 13:57:56 +0530

    adding rhe static changes

--- src/api/Controller/terms.js
+++ src/api/Controller/terms.js
@@ -2,7 +2,7 @@ const terms = require("../../model/terms");
 
 function create(req, res, next) {
   console.log(`body data ${JSON.stringify(req.body)}`);
-  const termsModel = new ContactUs(req.body);
+  const termsModel = new terms(req.body);
   termsModel
     .save()
     .then(() => res.json("terms saved successfully"))
